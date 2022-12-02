5070b99d09c419f8d2cb138d71e3494cbe7c36b3
Patrick Coral <patrick9999@hotmail.ch>
2022-12-02 01:16:56 +0100

    skills page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
-
+	ngOnInit() {
+		timer(300).subscribe(() => {
+			document.querySelectorAll('.splash-item').forEach((el) => {
+				el.classList.remove('hidden');
+			});
+		});
+	}
 }
