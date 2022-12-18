b924b08aa04cf5deb1bdcdae8161ff2ffb2acdb0
Patrick Coral <patrick9999@hotmail.ch>
2022-12-19 00:02:25 +0100

    Add boid behaviour

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,31 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import * as THREE from 'three';
-import {
-	ConeGeometry,
-	DirectionalLight,
-	DirectionalLightHelper,
-	Mesh,
-	MeshMatcapMaterial,
-	MeshNormalMaterial,
-	MeshStandardMaterial,
-	MeshToonMaterial,
-	Vector2,
-} from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
-import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass';
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 
 import * as YUKA from 'yuka';
-import {
-	EntityManager,
-	SeekBehavior,
-	SteeringBehavior,
-	SteeringManager,
-	Vehicle,
-	WanderBehavior,
-} from 'yuka';
 
 @Component({
 	selector: 'app-skills',
@@ -33,48 +9,137 @@ import {
 	styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent {
-	canvas: HTMLElement | undefined;
-	mousePos: [x: number, y: number] = [0, 0];
+	canvas: HTMLCanvasElement | undefined;
+	container?: HTMLElement;
 
-	readonly gltfLoader = new GLTFLoader();
 	readonly scene = new THREE.Scene();
-	camera: THREE.OrthographicCamera = new THREE.OrthographicCamera(
-		-2,
-		2,
-		2,
-		-2,
-		1,
-		1000
-	);
-	pCamera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, 1);
-	renderer: THREE.WebGL1Renderer | undefined;
-	composer: EffectComposer | undefined;
-	bloomPass: UnrealBloomPass = new UnrealBloomPass(
-		new Vector2(window.innerWidth, window.innerHeight),
-		0.35,
-		1.0,
-		0.8
-	);
-	renderPass: RenderPass | undefined;
-	SMAAPass: SMAAPass = new SMAAPass(1, 1);
+	camera?: THREE.PerspectiveCamera;
+	renderer?: THREE.WebGL1Renderer;
 
-	readonly icoGeo = new THREE.IcosahedronGeometry(5, 0);
-	readonly toonMat: MeshToonMaterial = new MeshToonMaterial({ color: 0x40e0d0 });
-	readonly mat = new MeshMatcapMaterial({color: 0xed37fa});
-	readonly icoSphere = new THREE.Mesh(this.icoGeo, this.mat)
-	
+	readonly coneGeo = new THREE.ConeGeometry(0.5, 2);
+	readonly normalMat = new THREE.MeshNormalMaterial();
+	readonly cone = new THREE.Mesh(this.coneGeo, this.normalMat);
 
-	readonly coneGeometry: ConeGeometry = new ConeGeometry(0.5, 2);
-	readonly normalMat: MeshNormalMaterial = new MeshNormalMaterial();
+	readonly entityManager = new YUKA.EntityManager();
+	readonly time = new YUKA.Time();
 
-	readonly dirLight: DirectionalLight = new DirectionalLight(0xffffff);
+	readonly path = new YUKA.Path();
 
-	readonly time: YUKA.Time = new YUKA.Time();
-	readonly entityManager: YUKA.EntityManager = new EntityManager();
-	readonly wanderBehavior: YUKA.WanderBehavior = new WanderBehavior();
-	readonly seekBehavior: YUKA.SeekBehavior = new SeekBehavior(
-		new YUKA.Vector3(0, 0, 0)
-	);
+	createPoint(range: number) {
+		return new YUKA.Vector3(
+			range / 2 - Math.random() * range,
+			range / 2 - Math.random() * range,
+			range / 2 - Math.random() * range
+		);
+	}
+
+	createLoop(amount: number) {
+		let prev = new YUKA.Vector3();
+		for (let i = 0; i < amount; i++) {
+			let pos: YUKA.Vector3 = this.createPoint(50);
+			while (prev.distanceTo(pos) < 10) pos = this.createPoint(50);
+			this.path.add(pos);
+
+			prev = pos;
+		}
+	}
+
+	createBoids(amount: number) {
+		const leader = new YUKA.Vehicle();
+		let leaderCone = new THREE.Mesh(this.coneGeo, this.normalMat);
+		leader.maxSpeed = 40;
+		leaderCone.matrixAutoUpdate = false;
+
+		leader.steering.add(new YUKA.FollowPathBehavior(this.path, 10));
+
+		this.entityManager.add(leader);
+
+		const separationBehavior = new YUKA.SeparationBehavior();
+		separationBehavior.weight = 10;
+
+		const pursuitBehavior = new YUKA.PursuitBehavior(leader);
+		pursuitBehavior.weight = 0.5;
+
+		for (let i = 0; i < amount; i++) {
+			const vehicle = new YUKA.Vehicle();
+			let cone = new THREE.Mesh(this.coneGeo, this.normalMat);
+			vehicle.setRenderComponent(cone, this.sync);
+			vehicle.maxSpeed = 20 + Math.random() * 20;
+			cone.matrixAutoUpdate = false;
+			this.scene.add(cone);
+
+			vehicle.updateNeighborhood = true;
+			vehicle.neighborhoodRadius = 5;
+
+			vehicle.position = this.createPoint(30);
+
+			vehicle.steering.add(pursuitBehavior);
+			vehicle.steering.add(new YUKA.AlignmentBehavior());
+			vehicle.steering.add(new YUKA.CohesionBehavior());
+			vehicle.steering.add(separationBehavior);
+
+			this.entityManager.add(vehicle);
+		}
+	}
+
+	createScene() {
+		if (this.canvas === undefined || this.container === undefined) return;
+
+		this.camera = new THREE.PerspectiveCamera(
+			50,
+			this.container.clientWidth / this.container.clientHeight
+		);
+
+		this.renderer = new THREE.WebGL1Renderer({
+			canvas: this.canvas,
+			alpha: true,
+			antialias: true,
+		});
+		this.renderer?.setSize(
+			this.container.clientWidth,
+			this.container.clientHeight,
+			true
+		);
+
+		this.camera.position.z = 100;
+
+		this.path.loop = true;
+
+		this.createLoop(5);
+
+		this.coneGeo.rotateX(Math.PI / 2);
+
+		this.createBoids(200);
+
+		this.renderer.render(this.scene, this.camera);
+	}
+
+	sync(entity: YUKA.GameEntity, renderComponent: any) {
+		renderComponent.matrix.copy(entity.worldMatrix);
+	}
+
+	animate = () => {
+		const delta = this.time.update().getDelta();
+		this.entityManager.update(delta);
+		if (this.camera !== undefined)
+			this.renderer?.render(this.scene, this.camera);
+		window.requestAnimationFrame(this.animate);
+	};
+
+	@HostListener('window:resize', ['$event'])
+	onResize() {
+		if (this.container === undefined || this.camera === undefined) return;
+		console.log('ping');
+
+		this.camera.aspect =
+			this.container?.clientWidth / this.container?.clientHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer?.setSize(
+			this.container.clientWidth,
+			this.container.clientHeight,
+			true
+		);
+	}
 
 	observer: IntersectionObserver = new IntersectionObserver((entries) => {
 		for (const entry of entries) {
@@ -84,93 +149,11 @@ export class SkillsComponent {
 		}
 	});
 
-	sync = (entity: YUKA.GameEntity, renderComponent: any) => {
-		renderComponent.matrix.copy(entity.worldMatrix);
-	};
-
-	createBoids(amount: number) {
-		this.coneGeometry.rotateX(Math.PI / 2);
-		for (let i = 0; i < amount; i++) {
-			let cone = new Mesh(this.coneGeometry, this.normalMat);
-			cone.matrixAutoUpdate = false;
-			this.scene.add(cone);
-
-			let vehicle: YUKA.Vehicle = new YUKA.Vehicle();
-			vehicle.setRenderComponent(cone, this.sync);
-			vehicle.rotation.fromEuler(
-				Math.PI * 2 - Math.random() * 4 * Math.PI,
-				Math.PI * 2 - Math.random() * 4 * Math.PI,
-				Math.PI * 2 - Math.random() * 4 * Math.PI
-			)
-			this.entityManager.add(vehicle);
-			vehicle.steering.add(this.wanderBehavior);
-			vehicle.smoother = new YUKA.Smoother(50);
-		}
-	}
-
-	createScene() {
-		this.canvas = document.getElementById('room') ?? undefined;
-		this.renderer = new THREE.WebGL1Renderer({
-			canvas: this.canvas,
-			alpha: true,
-			antialias: true,
-		});
-
-		this.renderer.setPixelRatio(window.devicePixelRatio);
-		this.renderer.setSize(
-			this.canvas?.clientWidth ?? 1,
-			this.canvas?.clientHeight ?? 1
-		);
-
-		this.pCamera.position.setY(30);
-		this.pCamera.rotateX(-Math.PI / 2);
-
-		this.dirLight.position.set(-5, 5, -5);
-
-		this.scene.add(this.icoSphere);
-		this.scene.background = new THREE.Color(0x131316);
-
-		this.createBoids(100);
-
-		this.composer = new EffectComposer(this.renderer);
-		this.renderPass = new RenderPass(this.scene, this.pCamera);
-		this.composer?.addPass(this.renderPass);
-		this.composer?.addPass(this.bloomPass);
-		this.composer.addPass(this.SMAAPass);
-	}
-
-	updateSteeringBehavior() {
-		this.entityManager.entities.forEach((entity) => {
-			if (entity instanceof Vehicle) {
-				if (entity.position.length() > 15) {
-					entity.steering
-						.remove(this.wanderBehavior)
-						.add(this.seekBehavior);
-				} else {
-					entity.steering
-						.remove(this.seekBehavior)
-						.add(this.wanderBehavior);
-				}
-			}
-		});
-	}
-
-	animate = () => {
-		let delta = this.time.update().getDelta();
-		this.entityManager.update(delta*10);
-		this.updateSteeringBehavior();
-		this.icoSphere.rotateZ(delta);
-		window.requestAnimationFrame(this.animate);
-		this.composer?.render();
-	};
-
-	@HostListener('document:mousemove', ['$event'])
-	onMouseMove(e: MouseEvent) {
-		this.mousePos[0] = e.clientX / window.innerWidth;
-		this.mousePos[1] = e.clientY / window.innerHeight;
-	}
-
 	ngOnInit() {
+		const canvas = document.getElementById('room') ?? undefined;
+		if (canvas instanceof HTMLCanvasElement) this.canvas = canvas;
+		this.container = document.getElementById('container3D')??undefined;
+
 		document
 			.querySelectorAll('.animated')
 			.forEach((e) => this.observer.observe(e));
