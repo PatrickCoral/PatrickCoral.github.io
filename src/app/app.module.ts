fc43b13fb83260eff841fffd2eec05914d1430e9
Patrick Coral <patrick9999@hotmail.ch>
2023-01-17 17:44:14 +0100

    New release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SkillsComponent } from './skills/skills.component';
-import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     SkillsComponent,
-    ContactComponent
   ],
   imports: [
     BrowserModule,
